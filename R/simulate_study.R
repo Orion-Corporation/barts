2f222f4992220b088c4a80baa2740b670e261cb3
Mikko Marttila <mikko.marttila@orionpharma.com>
2021-05-19 11:34:48 +0300

    Recycle arguments and add comments

--- R/simulate_study.R
+++ R/simulate_study.R
@@ -1,13 +1,20 @@
 #' Simulate a study
+#'
 #' @param n_max The number of subjects to stop after.
 #' @param theta A vector of true event probabilities in each arm.
 #' @param a A vector of "a" parameters for the beta prior.
 #' @param b A vector of "b" parameters for the beta prior.
 #' @param rule Treatment allocation rule to use.
 #' @param n_draws The number of draws to sample from the posterior for
-#'   evaluating posterior probabilities.
+#'   evaluating posterior probabilities in allocation rules.
+#' @param n_burn_in The number of subjects to allocate using `burn_in_rule` in
+#'   the beginning before swapping to `rule`.
+#' @param burn_in_rule Allocation rule to use during the burn-in period.
+#'
 #' @importFrom purrr %||%
+#' @importFrom zeallot %<-%
 #' @export
+#'
 #' @examples
 #' result <- simulate_study(
 #'   n_max = 240,
@@ -18,17 +25,25 @@
 #' )
 simulate_study <- function(n_max, theta, a, b, rule, n_draws = 3000,
                            n_burn_in = 0, burn_in_rule = randomization_list()) {
-  # Initialize list to hold tracked parameters at each step.
+  # Validate input parameters
+  c(theta, a, b) %<-% vctrs::vec_recycle_common(theta = theta, a = a, b = b)
+
+  # Initialize list to hold tracked parameters at each step -- one step before
+  # each subject and one after the last to evaluate the posterior in the end.
   trace <- vector("list", n_max + 1)
 
-  rules <- list(burn_in_rule, rule)
-  rules <- lapply(rules, rule_initialize, n_arms = length(theta), n_max = n_max)
+  # Initialize allocation rules
+  rules <- lapply(list(burn_in_rule, rule), function(r) {
+    rule_initialize(r, n_arms = length(theta), n_max = n_max)
+  })
 
+  # Set burn-in rule as initially active
   active_rule <- rules[[1L]]
   rule_updated <- FALSE
 
   for (i in seq_len(n_max + 1)) {
-    # Swap allocation rule after burn-in period
+    # Swap allocation rule after burn-in period -- rules are passed by value
+    # so need to make sure to only do this once rather than at each iteration.
     if (i > n_burn_in && !rule_updated) {
       active_rule <- rules[[2L]]
       rule_updated <- TRUE
